6de68593ba13a629e637cb4b094e6486f4cf6b3d
Jackson Milner <jackson.milner@wsu.edu>
2021-12-01 16:31:21 -0800

    Add project files.

---
+++ Pong/main.cpp
@@ -0,0 +1,31 @@
+// This example will use SFML libraries to implement
+// the game of Pong
+
+// History: 4/16 - Started from the SFML tutorial code that draws
+//                a circle in the window.
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	sf::CircleShape shape(100.f);
+
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
