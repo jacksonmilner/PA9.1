a6343757d55b0df1e09ffed6e73a43a929cb967e
Bradley Cruz <bradley.ty.cruz@gmail.com>
2021-12-03 18:14:54 -0800

    bru

--- Pong/mainShip.h
+++ Pong/mainShip.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "ship.h"
 
-class mainShip : public spaceShip
+class spaceShip : public sf::CircleShape
 {
 public:
+	spaceShip(const double& newRadius, const sf::Color& newColor,
+		const sf::Vector2f& newPosition) :
+		sf::CircleShape(newRadius)
+	{
+		this->setFillColor(newColor);
+		this->setPosition(newPosition);
+	}
 
 private:
-
+	// double mRadius;
 };
--- Pong/ship.h
+++ Pong/ship.h
@@ -1,17 +1,3 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-class spaceShip : public sf::CircleShape
-{
-public:
-	spaceShip(const double& newRadius, const sf::Color& newColor,
-		const sf::Vector2f& newPosition) :
-		sf::CircleShape(newRadius)
-	{
-		this->setFillColor(newColor);
-		this->setPosition(newPosition);
-	}
-
-private:
-	// double mRadius;
-};
