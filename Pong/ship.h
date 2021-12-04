024178a2ea0cd73652eeb53bf0146104f16390c8
owen32ryan <owen.ryan@wsu.edu>
2021-12-03 18:10:26 -0800

    commit

--- Pong/ship.h
+++ Pong/ship.h
@@ -1,17 +1,2 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-
-class spaceShip : public sf::CircleShape
-{
-public:
-	spaceShip(const double& newRadius, const sf::Color& newColor,
-		const sf::Vector2f& newPosition) :
-		sf::CircleShape(newRadius)
-	{
-		this->setFillColor(newColor);
-		this->setPosition(newPosition);
-	}
-	bool isInbounds(char side);
-private:
-	// double mRadius;
-};
