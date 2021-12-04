ccb2adb41b148fc81c150f96855b65a0cbe32431
Bradley Cruz <bradley.ty.cruz@gmail.com>
2021-12-03 18:08:00 -0800

    bruh

--- Pong/mainShip.h
+++ Pong/mainShip.h
@@ -1,17 +1,10 @@
 #pragma once
 #include "ship.h"
 
-class spaceShip : public sf::CircleShape
+class mainShip : public spaceShip
 {
 public:
-	spaceShip(const double& newRadius, const sf::Color& newColor,
-		const sf::Vector2f& newPosition) :
-		sf::CircleShape(newRadius)
-	{
-		this->setFillColor(newColor);
-		this->setPosition(newPosition);
-	}
 
 private:
-	// double mRadius;
+
 };
