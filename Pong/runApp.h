eba9fe405107f3f0a57904cf28a0f9592699d5db
Bradley Cruz <bradley.ty.cruz@gmail.com>
2021-12-07 17:21:05 -0800

    added bugs/working on runapp(maybe will scratch)

--- Pong/runApp.h
+++ Pong/runApp.h
@@ -7,7 +7,90 @@
 class RunApp
 {
 public:
+	RunApp();
+	~RunApp();
 
+	void initWindow();
+	void initVars();
+	void update();
+	void render();
+	bool isRunning();
+	void inputCheck();
+	void printStartScreen();
 private:
+
+
+	sf::RenderWindow* mWindow;
+	sf::VideoMode mVideoMode;
+	sf::Event mEvent;
+	spaceShip mPlayer;
+	bool gameRunning;
 	int score;
 };
+
+RunApp::RunApp()
+{
+	initWindow();
+	initVars();
+}
+
+RunApp::~RunApp()
+{
+	delete mWindow;
+	mWindow = nullptr;
+}
+
+void RunApp::initWindow()
+{
+	mVideoMode = sf::VideoMode(500, 500);
+	mWindow = new sf::RenderWindow(mVideoMode, "Invaders");
+}
+
+void RunApp::initVars()
+{
+	return;
+}
+
+void RunApp::update()
+{
+	inputCheck();
+}
+
+void RunApp::render()
+{
+	mWindow->clear();
+
+
+	mWindow->display();
+}
+
+bool RunApp::isRunning()
+{
+	gameRunning = mWindow->isOpen();
+	return gameRunning;
+}
+
+void RunApp::inputCheck()
+{
+	while (mWindow->pollEvent(mEvent))
+	{
+		switch (mEvent.type)
+		{
+		case sf::Event::Closed:
+			{
+			mWindow->close();
+			break;
+			}
+		default:
+			break;
+		}
+	}
+}
+
+void RunApp::printStartScreen()
+{
+	if (gameRunning == true)
+	{
+
+	}
+}
---
+++ Pong/spaceShip.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class spaceShip2
+{
+public:
+	spaceShip2();
+	~spaceShip2();
+
+	void initShape();
+
+private:
+	sf::CircleShape mShape;
+	void update();
+	void render(sf::RenderTarget* target);
+};
+
+spaceShip2::spaceShip2()
+{
+
+}
+
+spaceShip2::~spaceShip2()
+{
+
+}
+
+void spaceShip2::initShape()
+{
+	mShape.setFillColor(sf::Color::Yellow);
+	mShape.setRadius(15);
+	mShape.setPosition(sf::Vector2f(window.getSize().x / 2, window.getSize().y - 90)))
+}
+
+void spaceShip2::update()
+{
+
+}
+
+void spaceShip2::render(sf::RenderTarget* target)
+{
+	target->draw(mShape);
+}
